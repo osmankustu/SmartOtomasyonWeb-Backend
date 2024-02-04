f77b8cb710548717192f992d9a90dcd9bfa457ce
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2024-02-04 23:17:51 +0300

    Update ApplicationDbContext.cs

--- src/Infrastructure/SmartOtomasyonWebApp.Persistance/Context/ApplicationDbContext.cs
+++ src/Infrastructure/SmartOtomasyonWebApp.Persistance/Context/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace SmartOtomasyonWebApp.Persistance.Context
             //local
             //optionsBuilder.UseSqlServer(@"Server=DESKTOP-N6K9MD1;Database=SmartOtomasyonWeb;Trusted_Connection=true;TrustServerCertificate=True");
             //production
-            optionsBuilder.UseSqlServer(@"Server=SmartOtomasyonWeb.mssql.somee.com;Database=SmartOtomasyonWeb;user id=DEVSOFT_SQLLogin_1;pwd=fjuezb1tyi;Trusted_Connection=false;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(@"Server=*;Database=SmartOtomasyonWeb;user id=*;pwd=*;Trusted_Connection=false;TrustServerCertificate=True");
             base.OnConfiguring(optionsBuilder);
 
         }
