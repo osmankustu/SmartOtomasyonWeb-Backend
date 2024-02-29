7eb7e4319f45662959578cdf3db3e996a31b3c24
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2024-02-29 18:12:10 +0300

    development

--- src/Infrastructure/SmartOtomasyonWebApp.Persistance/Context/ApplicationDbContext.cs
+++ src/Infrastructure/SmartOtomasyonWebApp.Persistance/Context/ApplicationDbContext.cs
@@ -14,9 +14,8 @@ namespace SmartOtomasyonWebApp.Persistance.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //local
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-N6K9MD1;Database=SmartOtomasyonWeb;Trusted_Connection=true;TrustServerCertificate=True");
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-N6K9MD1;Database=SmartOtomasyonWeb;Trusted_Connection=true;TrustServerCertificate=True");
             //production
-            optionsBuilder.UseSqlServer(@"Server=SmartOtomasyonWeb.mssql.somee.com;Database=SmartOtomasyonWeb;user id=DEVSOFT_SQLLogin_1;pwd=fjuezb1tyi;Trusted_Connection=false;TrustServerCertificate=True");
             base.OnConfiguring(optionsBuilder);
 
         }
